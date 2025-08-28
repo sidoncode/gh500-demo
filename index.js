dc5b3292b506906bd438b141ab6844e4b746f79d
Siddharth Sharma <siddharth54330@gmail.com>
2025-08-28 23:57:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from GitHub Actions + Dependabot!");
+  res.send("Hello from GitHub Actions + Dependabot! commit-2");
 });
 
 app.listen(port, () => {
